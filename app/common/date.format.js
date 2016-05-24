ee3f2709ba65893a77fde5338bec86c3d4be69b1
TonyChyi <tonychee1989@gmail.com>
2016-05-24 12:53:11 +0800

    time

--- app/common/date.format.js
+++ app/common/date.format.js
@@ -1,14 +1,14 @@
 'use strict';
 
 Date.prototype.formatRFC3339 = function() {
-  return "{0}-{1}-{2}T{3}:{4}:{5}Z{6}".format(
+  return "{0}-{1}-{2}T{3}:{4}:{5}{6}".format(
     this.getFullYear().leadingZero(),
     this.getMonth().leadingZero(),
     this.getDate().leadingZero(),
     this.getHours().leadingZero(),
     this.getMinutes().leadingZero(),
     this.getSeconds().leadingZero(),
-    "{0}:{1}".format(
+    this.getTimezoneOffset() == 0 ? 'Z' : "{0}:{1}".format(
       (-this.getTimezoneOffset() / 60).leadingZero(), (-this.getTimezoneOffset() %
         60).leadingZero()
     )
